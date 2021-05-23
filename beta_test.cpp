e40137935e346159ff55de41458b8bb96d4f8ed5
Sadie Thomas <sadierosethomas@gmail.com>
2021-05-23 13:33:21 -0700

    initial commit

---
+++ beta_test.cpp
@@ -0,0 +1,67 @@
+
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+
+using namespace std;
+
+
+int main(){
+	
+	int correct_net = 0;
+	int wrong_net = 0;
+	int correct_rim = 0;
+	int wrong_rim = 0;
+	int questionable = 0;
+
+	cout << "\ncn/a = correct net\nwn/s = wrong net\ncr/d = correct rim\nwr/f = wrong rim\nq = questionable\nendgame = end program\n" << endl;
+	string input;
+	while(1){
+		cout << "Enter: ";
+		cin >> input;
+		if(input == "cn" || input == "a"){
+			correct_net++;
+		}
+		else if(input == "wn" || input == "s"){
+			wrong_net++;
+		}
+		else if(input == "cr" || input == "d"){
+			correct_rim++;
+		}
+		else if(input == "wr" || input == "f"){
+			wrong_rim++;
+		}
+		else if(input == "q"){
+			questionable++;
+		}
+		else if(input == "endgame"){
+			break;
+		}
+		else{
+			cout << "Invalid input!" << endl;
+		}
+	}
+
+	ofstream fout;
+	fout.open("accuracy.txt", ios::out | ios:: app);
+	
+	time_t now = time(0);
+
+	fout << ctime(&now);
+	fout << "Total shots: " << correct_net + wrong_net + correct_rim + wrong_rim << endl;
+	fout << "\tCorrect net: " << correct_net << endl;
+	fout << "\tWrong net: " << wrong_net << endl;
+	fout << "\tCorrect rim: " << correct_rim << endl;
+	fout << "\tWrong rim: " << wrong_rim << endl;
+	fout << "\tQuestionable: " << questionable << endl << endl;
+	fout << "\tNet accuracy: " << float (correct_net)/(correct_net + wrong_net) << endl;
+	fout << "\tRim accuracy: " << float (correct_rim)/(correct_rim + wrong_rim) << endl;
+	fout << "\tTotal accuracy: " << float (correct_net + correct_rim)/(correct_net + wrong_net + correct_rim + wrong_rim) << endl << endl << endl;
+
+	fout.close();
+
+
+
+	return 0;
+}
---
+++ parse_files.cpp
@@ -0,0 +1,226 @@
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+
+using namespace std;
+
+#define SAMPLES 450
+
+bool open_file(ifstream&, string);
+bool verify_files(ifstream&, ifstream&);
+void write_to_file(ifstream&, ofstream&);
+
+
+int main(int argc, char** argv){
+	ifstream fin_data;
+	ifstream fin_keys;
+	string data_name;
+	string keys_name;
+
+	//get file names from command line or user input
+	if(argc == 3){
+		data_name = string(argv[1]);
+		keys_name = string(argv[2]);
+	}
+	else{
+		cout << "Enter data file name: ";
+		cin >> data_name;
+		cout << "Enter key file name: ";
+		cin >> keys_name;
+	}
+
+	//make sure files open properly
+	if(!open_file(fin_data, data_name) || !open_file(fin_keys, keys_name)){
+		return 1;
+	}	
+
+	//ensure proper formatting in both files
+	try{
+		cout << "\nVerifying files..." << endl;
+		if(!verify_files(fin_data, fin_keys)){
+			return 1;
+		}
+	}catch(invalid_argument& ia){
+		cout << ia.what() << endl;
+		fin_data.close();
+		fin_keys.close();
+		return 1;
+	}
+
+	//close and re-open files to start at beginning
+	fin_data.close();
+	fin_keys.close();
+	if(!open_file(fin_data, data_name) || !open_file(fin_keys, keys_name)){
+		return 1;
+	}
+
+	//open output files to append
+	ofstream fout_net;
+	ofstream fout_rim;
+	fout_net.open("net_shot.csv", ios::out | ios::app);
+	fout_rim.open("rim_shot.csv", ios::out | ios::app);
+
+	int key;
+	try{	
+		cout << endl;
+		fin_keys >> key;
+		while(!fin_keys.eof()){
+			if(key==0){
+				cout << "Writing to net file" << endl;
+				write_to_file(fin_data, fout_net);
+			}
+			else if(key==1){
+				cout << "Writing to rim file" << endl;
+				write_to_file(fin_data, fout_rim);
+			}
+			else if(key==-1){
+				cout << "Trashing output" << endl;
+				string trash;
+				for(int i=0; i<(SAMPLES*3+1); i++){
+					fin_data >> trash;
+				}
+			}
+			else{
+				cout << "\tERROR: unidentified key" << endl;
+				return 1;	
+			}
+			fin_keys >> key;
+		}
+	}catch(invalid_argument& ia){
+		cout << ia.what() << endl;
+	}
+
+	//read in one more piece of data so eof flag is set
+	fin_data >> key;
+
+	cout << "\nDone writing output" << endl;
+
+	//both files should be at the end, otherwise there was an error
+	if(fin_data.eof() && fin_keys.eof()){
+		cout << "Successful write" << endl;
+	}
+	else{
+		cout << "\tERROR: mismatch in end of file flags" << endl;
+		cout << "\tfin_keys.eof(): " << fin_keys.eof() << endl;
+		cout << "\tfin_data.eof(): " << fin_data.eof() << endl;
+	}	
+
+	//close file streams
+	fin_data.close();
+	fin_keys.close();
+	fout_net.close();
+	fout_rim.close();
+
+
+	return 0;
+}
+
+
+bool open_file(ifstream& fin, string file_name){
+	cout << "Opening " << file_name << endl;
+	fin.open(file_name);
+	if(!fin.is_open()){
+		cout << "\tError opening file" << endl;
+		return 0;
+	}
+	return 1;
+}
+
+
+bool verify_files(ifstream& fin_data, ifstream& fin_keys){
+	int net = 0;
+	int rim = 0;
+	int trash = 0;
+	string data;
+	int key;
+
+	fin_keys >> key;
+	while(!fin_keys.eof()){
+		//count how many of each shot
+		if(key==0){
+			net++;
+		}
+		else if(key==1){
+			rim++;
+		}
+		else if(key==-1){
+			trash++;
+		}
+		else{
+			cout << "\tFound key: " << key << endl;
+			cout << "\tError on sample number " << net+rim+trash << endl;
+			throw invalid_argument("\tERROR: Invalid key detected");
+		}
+
+		//make sure all data is present
+		for(int i=0; i<(3*SAMPLES); i++){
+			fin_data >> data;
+			if(fin_data.eof()){
+				if(i==0){
+					cout << "\tFailed on first line of sample" << endl;
+				}
+				cout << "\tError on sample number " << net+rim+trash << endl;
+				throw invalid_argument("\tERROR: End of data file.");
+			}
+			if(data == "End"){
+				cout << "\tError on sample number " << net+rim+trash << endl;
+				throw invalid_argument("\tERROR: End of sample message found too early");
+			}
+		}
+
+		//check for marker signifying end of sample
+		fin_data >> data;
+		if(data != "End"){
+			cout << "\tError on sample number " << net+rim+trash << endl;
+			throw invalid_argument("\tERROR: End of sample message not found");
+		}
+
+		fin_keys >> key;
+	}
+
+	//read extra data in to set eof flag
+	fin_data >> data;
+
+	//make sure both files are at the end
+	if(fin_keys.eof() && fin_data.eof()){
+		cout << "Files verified. Found " << net+rim+trash << " samples" << endl;
+		cout << "\tNet: " << net << endl;
+		cout << "\tRim: " << rim << endl;
+		cout << "\tTrash: " << trash << endl << endl;
+		return 1;
+	}
+	if(fin_keys.eof()){
+		cout << "\tERROR: too much information in data file or not enough in keys file" << endl;
+	}
+	if(fin_data.eof()){
+		cout << "\tERROR: not enough information in data file or too much in keys file" << endl;
+	}
+	return 0;
+}
+
+
+void write_to_file(ifstream& fin, ofstream& fout){
+	string num;
+	for(int i=0; i<450; i++){
+		for(int j=0; j<3; j++){
+			fin >> num;
+			if(fin.eof()){
+				throw invalid_argument("\tERROR: End of data file. Not enough information in data file or too much in keys file.");
+			}
+			if(num == "End"){
+				throw invalid_argument("\tERROR: End of sample message found too early");
+			}
+			fout << num;
+		}
+		fout << endl;
+	}
+	fout << endl;
+
+	//check for marker signifying end of sample
+	fin >> num;
+	if(num != "End"){
+		throw invalid_argument("\tERROR: End of sample message not found");
+	}
+}
+
