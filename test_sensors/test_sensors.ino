fa35ffbb9e425d15226c58d8b9a58fe2b7403c24
sadiet20 <sadierosethomas@gmail.com>
2022-10-26 11:41:23 -0700

    wait for impact, print 500

--- test_sensors/test_sensors.ino
+++ test_sensors/test_sensors.ino
@@ -1,53 +1,65 @@
-const int microphonePin = A6;
+
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL343.h>
 
-#define ADXL343_SCK 13
-#define ADXL343_MISO 12
-#define ADXL343_MOSI 11
-#define ADXL343_CS 10
+//sensor pins
+//#define micPin A6;
+#define PIEZO_PIN1 A1
+#define PIEZO_PIN2 A2
 
-Adafruit_ADXL343 accel = Adafruit_ADXL343(12345);
-
+//readings from sensors
 float micro = 0;
 int Piezo1 = 0;
 int Piezo2 = 0;
 float RMS = 0;
 sensors_event_t event;
 
+//create accelerometer object with unique ID
+Adafruit_ADXL343 accel = Adafruit_ADXL343(12345);
+
+unsigned int id_num = 0;
+
 void setup() {
   Serial.begin(115200);
 
   if(!accel.begin())
   {
-    /* There was a problem detecting the ADXL343 ... check your connections */
+    // There was a problem detecting the ADXL343 ... check your connections
     Serial.println("Ooops, no ADXL343 detected ... Check your wiring!");
     while(1);
   }
-  
-  accel.setRange(ADXL343_RANGE_2_G);
+
+  //set maximum for accelerometer readings
+  accel.setRange(ADXL343_RANGE_8_G);
+
+  Serial.println("Time,Piezo1,Piezo2,RMS");
 }
 
 void loop() {
-  Piezo1 = analogRead(A1);
-  Piezo2 = analogRead(A2);
-  accel.getEvent(&event);
-  RMS = sqrt((sq(event.acceleration.x) + sq(event.acceleration.y) + sq(event.acceleration.z))/3);
+  //wait for vibration
+  while(analogRead(PIEZO_PIN1) < 1);
 
-  //Serial.print(millis());
-  //Serial.print(",");
-  Serial.print(Piezo1);
-  Serial.print(",");
-  Serial.print(Piezo2);
-  Serial.print(",");
-  Serial.println(RMS);
+  //get 500 readings
+  for(int i=0; i<500; i++) {
+    Piezo1 = analogRead(PIEZO_PIN1);
+    Piezo2 = analogRead(PIEZO_PIN2);
+    accel.getEvent(&event);
+    RMS = sqrt((sq(event.acceleration.x) + sq(event.acceleration.y) + sq(event.acceleration.z))/3);
+
+    Serial.print(millis());
+    Serial.print(",");
+    Serial.print(Piezo1);
+    Serial.print(",");
+    Serial.print(Piezo2);
+    Serial.print(",");
+    Serial.println(RMS);
+  }
+
+  //print unique identifier after each shot
+  Serial.print("#");
+  Serial.println(id_num);
+  id_num++;
 
 }
-  //micro = analogRead(microphonePin);
-  //Serial.print("Microphone:");
-  //Serial.print(micro);
-  //Serial.print(",");
-  //Serial.print("P1:");
-    //Serial.print("Accel:");
-      //Serial.print("P2:");
