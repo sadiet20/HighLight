5143616ed858a4f484d6f5fa5007b1cdcbabed8b
sadiet20 <sadierosethomas@gmail.com>
2022-11-01 17:25:21 -0700

    added more sensors and file/plot print versions

--- test_sensors/test_sensors.ino
+++ test_sensors/test_sensors.ino
@@ -6,13 +6,19 @@
 
 //sensor pins
 //#define micPin A6;
-#define PIEZO_PIN1 A1
-#define PIEZO_PIN2 A2
+#define PIEZO_PINRD1 A1
+#define PIEZO_PINRD2 A2
+#define PIEZO_PINSM A3
+#define PIEZO_PINLG A2
+#define PIEZO_PINLITE A4
 
 //readings from sensors
-float micro = 0;
-int Piezo1 = 0;
-int Piezo2 = 0;
+//float micro = 0;
+int PiezoRd1 = 0;
+int PiezoRd2 = 0;
+int PiezoSm = 0;
+int PiezoLg = 0;
+int PiezoLite = 0;
 float RMS = 0;
 sensors_event_t event;
 
@@ -34,27 +40,25 @@ void setup() {
   //set maximum for accelerometer readings
   accel.setRange(ADXL343_RANGE_8_G);
 
-  Serial.println("Time,Piezo1,Piezo2,RMS");
+  //Serial.println("Time,PiezoRd1,PiezoRd2,RMS,PiezoSm,PiezoLg,PiezoLite");
+  Serial.println("PiezoRdRim,PiezoRdLeg,RMS");
 }
 
 void loop() {
   //wait for vibration
-  while(analogRead(PIEZO_PIN1) < 1);
+  while(analogRead(PIEZO_PINRD1) < 1);
 
   //get 500 readings
   for(int i=0; i<500; i++) {
-    Piezo1 = analogRead(PIEZO_PIN1);
-    Piezo2 = analogRead(PIEZO_PIN2);
+    PiezoRd1 = analogRead(PIEZO_PINRD1);
+    PiezoRd2 = analogRead(PIEZO_PINRD2);
     accel.getEvent(&event);
     RMS = sqrt((sq(event.acceleration.x) + sq(event.acceleration.y) + sq(event.acceleration.z))/3);
+    PiezoSm = analogRead(PIEZO_PINSM);
+    PiezoLg = analogRead(PIEZO_PINLG);
+    PiezoLite = analogRead(PIEZO_PINLITE);
 
-    Serial.print(millis());
-    Serial.print(",");
-    Serial.print(Piezo1);
-    Serial.print(",");
-    Serial.print(Piezo2);
-    Serial.print(",");
-    Serial.println(RMS);
+    plot_print_all();
   }
 
   //print unique identifier after each shot
@@ -62,4 +66,46 @@ void loop() {
   Serial.println(id_num);
   id_num++;
 
+}
+
+void plot_print_all(){
+  Serial.print("PiezoRd1Rim:");   
+  Serial.print(PiezoRd1);
+  Serial.print(",");
+  Serial.print("PiezoRd2Leg:");
+  Serial.print(PiezoRd2);
+  Serial.print(",");
+  Serial.print("RMS:");
+  Serial.println(RMS);
+
+  /*
+  Serial.print(",");
+  Serial.print("PiezoSm:");   
+  Serial.print(PiezoSm);
+  Serial.print(",");
+  Serial.print("PiezoLg:");   
+  Serial.print(PiezoLg);
+  Serial.print(",");
+  Serial.print("PiezoLite:");   
+  Serial.println(PiezoLite);
+  */
+}
+
+void file_print_all(){
+  Serial.print(millis());
+  Serial.print(",");
+  Serial.print(PiezoRd1);
+  Serial.print(",");
+  Serial.print(PiezoRd2);
+  Serial.print(",");
+  Serial.println(RMS);
+
+  /*
+  Serial.print(",");
+  Serial.print(PiezoSm);
+  Serial.print(",");  
+  Serial.print(PiezoLg);
+  Serial.print(",");  
+  Serial.println(PiezoLite);
+ */
 }
